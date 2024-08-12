5ec0038c959533a72ea03f082f7827d428674c26
san43zko <san43zko@gmail.com>
2024-08-12 22:18:14 +0200

    huy5

--- docs/docs.go
+++ docs/docs.go
@@ -18,7 +18,7 @@ const docTemplate = `{
     "paths": {
         "/auth/login": {
             "get": {
-                "description": "Redirects the client to the Steam authentication page using the Steam API.",
+                "description": "Redirects the client to the Steam authentication page using the Steam API 123.",
                 "consumes": [
                     "application/json"
                 ],
