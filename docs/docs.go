56334385e2dea0b8fc9c5896a898717dcb1827d6
SAN43ZKO <admin@san43z.online>
2024-08-13 13:26:28 +0200

    huy24

--- docs/docs.go
+++ docs/docs.go
@@ -27,7 +27,7 @@ const docTemplate = `{
                 "tags": [
                     "auth"
                 ],
-                "summary": "Redirects client to Steam authentication page 123",
+                "summary": "Redirects client to Steam authentication page 12",
                 "responses": {
                     "302": {
                         "description": "Found"
